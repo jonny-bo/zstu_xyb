38694beffa06df5fa96485e02698c78b0e1ed50e
lijiangbo <lijiangbo@howzhi.com>
2017-04-21 19:52:45 +0800

    增加小组的开启关闭和修改图片

--- src/Biz/Group/Service/GroupService.php
+++ src/Biz/Group/Service/GroupService.php
@@ -15,4 +15,40 @@ interface GroupService
     public function updateGroup($groupId, $fields);
 
     public function deleteGroup($groupId);
+
+    public function closeGroup($groupId);
+
+    public function openGroup($groupId);
+
+    public function changeGroupImg($groupId, $field, $data);
+
+    // public function joinGroup($groupId, $userId);
+
+    // public function exitGroup($groupId, $userId);
+
+    // public function findGroupsByUserId($userId);
+
+    // public function searchMembers($conditions, $orderBy, $start, $limit);
+
+    // public function searchMembersCount($conditions);
+
+    // public function isOwner($groupId, $userId);
+
+    // public function isAdmin($groupId, $userId);
+
+    // public function isMember($groupId, $userId);
+
+    // public function addOwner($groupId, $userId);
+
+    // public function updateMember($memberId, $fields);
+
+    // public function getMembersCountByGroupId($groupId);
+
+    // public function getMemberByGroupIdAndUserId($groupId, $userId);
+
+    // public function deleteMemberByGroupIdAndUserId($groupId, $userId);
+
+    // public function waveGroup($groupId, $field, $diff);
+
+    // public function waveMember($groupId, $userId, $field, $diff);
 }
--- src/Biz/Group/Service/Impl/GroupServiceImpl.php
+++ src/Biz/Group/Service/Impl/GroupServiceImpl.php
@@ -4,6 +4,13 @@ namespace Biz\Group\Service\Impl;
 
 use Biz\Common\BaseService;
 use Biz\Group\Service\GroupService;
+use Biz\Common\ArrayToolkit;
+use Biz\Common\SimpleValidator;
+use Biz\Common\Exception\InvalidArgumentException;
+use Biz\Common\Exception\UnexpectedValueException;
+use Biz\Common\Exception\ResourceNotFoundException;
+use Biz\Common\Exception\RuntimeException;
+use Biz\Common\Exception\AccessDeniedException;
 
 class GroupServiceImpl extends BaseService implements GroupService
 {
@@ -29,12 +36,64 @@ class GroupServiceImpl extends BaseService implements GroupService
 
     public function updateGroup($groupId, $fields)
     {
+        if (isset($fields['about'])) {
+            $fields['about']=$this->purifyHtml($fields['about']);
+        }
         return $this->getGroupDao()->update($groupId, $fields);
     }
 
     public function deleteGroup($groupId)
     {
         return $this->getGroupDao()->delete($groupId);
+    }
+
+    public function closeGroup($groupId)
+    {
+        return $this->updateGroup($groupId, array(
+            'status' => 'close',
+        ));
+    }
+
+    public function openGroup($groupId)
+    {
+        return $this->updateGroup($groupId, array(
+            'status' => 'open',
+        ));
+    }
+
+    public function changeGroupImg($groupId, $field, $data)
+    {
+        if (!in_array($field, array('logo', 'backgroundLogo'))) {
+            throw new InvalidArgumentException('更新的字段错误！');
+        }
+
+        $group=$this->getGroup($groupId);
+        if (empty($group)) {
+            throw new ResourceNotFoundException('小组', $groupId);
+        }
+
+        $fileIds = ArrayToolkit::column($data, 'id');
+        $files = $this->getFileService()->findFilesByIds($fileIds);
+
+        $files = ArrayToolkit::index($files, "id");
+        $fileIds = ArrayToolkit::index($data, "type");
+
+        $fields = array(
+            $field => $files[$fileIds[$field]["id"]]["uri"],
+        );
+
+        $oldAvatars = array(
+            $field => $group[$field] ? $group[$field] : null,
+        );
+
+        $fileService = $this->getFileService();
+        array_map(function ($oldAvatar) use ($fileService) {
+            if (!empty($oldAvatar)) {
+                $fileService->deleteFileByUri($oldAvatar);
+            }
+        }, $oldAvatars);
+
+        return $this->getGroupDao()->updateGroup($groupId, $fields);
     }
 
     protected function getGroupDao()
