004be7f727721ba235de201f8b662ec172232b98
lijiangbo <lijiangbo@howzhi.com>
2017-04-27 21:48:10 +0800

    用户添加显示

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -37,6 +37,35 @@ class UserController extends BaseController
         ));
     }
 
+    public function createAction(Request $request)
+    {
+        if ($request->getMethod() == 'POST') {
+            $fields = $request->request->all();
+            $this->getUserService->register($fields);
+
+            return $this->redirect($this->generateUrl('admin_user'));
+        }
+        return $this->render('AppBundle:admin/user:create-modal.html.twig');
+    }
+
+    public function checkUsernameAction(Request $request)
+    {
+        $username = $request->request->get('username');
+
+        $res = $this->getUserService()->isUsernameAvaliable($username);
+
+        return $this->createJsonResponse(array('success' => $res));
+    }
+
+    public function checkEmailAction(Request $request)
+    {
+        $email = $request->request->get('email');
+
+        $res = $this->getUserService()->isEmailAvaliable($email);
+
+        return $this->createJsonResponse(array('success' => $res));
+    }
+
     protected function getUserService()
     {
         return $this->biz->service('User:UserService');
